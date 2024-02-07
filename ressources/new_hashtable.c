1da6398285c470ebd766e50778a37211bb9c9d32
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-02-07 16:45:44 +0100

    [+] adding nex_hashtable function

---
+++ ressources/new_hashtable.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LIL-1-1-secured-nicolas.dumetz
+** File description:
+** new_hashtable.c
+*/
+
+#include "../include/hashtable.h"
+#include <stdlib.h>
+
+hashtable_t *new_hashtable(int (*hash)(char *, int), int len)
+{
+    hashtable_t *new;
+    int i;
+
+    new = malloc(sizeof(hashtable_t) * (len + 1));
+    for (i = 0; i < len; i++)
+        new[i].hash_data = -1;
+    new[i].hash_data = -1000;
+    return new;
+}
