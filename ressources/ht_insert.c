184992c39dbc5d28c0210153a72dd4279c4806ff
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-02-07 16:46:03 +0100

    [+] adding case when the hashing code is already used

--- ressources/ht_insert.c
+++ ressources/ht_insert.c
@@ -16,6 +16,20 @@ static unsigned int hashtable_size(hashtable_t *ht)
     return i;
 }
 
+static int existing_hash(hashtable_t *ht, int steak, unsigned int modulo,
+    char *value)
+{
+    for (hashtable_t *current = ht[modulo].next; current;
+    current = current->next) {
+        if (current->hash_data == steak) {
+            free(current->value);
+            current->value = my_strdup(value);
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int ht_insert(hashtable_t *ht, char *key, char *value)
 {
     unsigned int size = hashtable_size(ht);
@@ -26,11 +40,13 @@ int ht_insert(hashtable_t *ht, char *key, char *value)
     if (size == 0)
         return 84;
     steak = ht[0].hash(key, size);
+    modulo = steak % size;
+    if (existing_hash(ht, steak, modulo, value))
+        return 0;
     new = malloc(sizeof(hashtable_t));
     new->hash = ht[0].hash;
     new->hash_data = steak;
     new->value = my_strdup(value);
-    modulo = steak % size;
     new->next = ht[modulo].next;
     ht[modulo].next = new;
     return 0;
